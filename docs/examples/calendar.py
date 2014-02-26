954a5959c2fcaa8c2bedb717739718862720f168
ndparker <ndparker@users.noreply.github.com>
2014-02-26 16:38:06 +0100

    dynamic calendar as main example.

--- docs/examples/calendar.py
+++ docs/examples/calendar.py
@@ -7,32 +7,57 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(os.path.normpath(__file__))))
 
 # BEGIN INCLUDE
-from tdi import html
+import datetime as _dt
+
+from tdi import html as _html
+from tdi.tools import html as _html_tools
+
 
 class Model(object):
-    def __init__(self, name, table):
-        self.name = name
-        self.table = table
+    MONTHS = u"""
+        Januar Februar M\xe4rz April Mai Juni Juli August September Oktober
+        November Dezember
+    """.split()
+
+    def __init__(self, year, month):
+        self.first = start = _dt.date(year, month, 1)
+        self.end = (start + _dt.timedelta(31)).replace(day=1)
+        self.name = self.MONTHS[month - 1]
+        self.year = year
+        self.start = start - _dt.timedelta(start.weekday())
+        self.today = _dt.date.today()
 
     def render_title(self, node):
-        node.content = self.name
+        node.content = u"%s %04d" % (self.name, self.year)
 
     def render_row(self, node):
-        node.repeat(None, self.table)
+        weeks, rest = divmod((self.end - self.start).days, 7)
+        weeks += bool(rest)
+        # repeat over each displayed week (each monday)
+        node.repeat(None, (self.start + _dt.timedelta(week * 7)
+            for week in xrange(weeks)
+        ))
 
     def render_col(self, node):
-        cols = node.ctx[1]
-        node.repeat(self.repeat_col, cols)
+        monday = node.ctx[1]
+        # repeat over each weekday
+        node.repeat(self.repeat_col, (monday + _dt.timedelta(day)
+            for day in xrange(7)
+        ))
 
-    def repeat_col(self, node, col):
-        node.content = col
+    def repeat_col(self, node, date):
+        node.content = date.day
+        if date.weekday() >= 5:
+            _html_tools.class_add(node, u'weekend')
+        if not(self.first <= date < self.end):
+            _html_tools.class_add(node, u'fill')
+        if date == self.today:
+            _html_tools.class_add(node, u'today')
 
-table = [
-    ( 1,  2,  3,   4,   5,   6,   7),
-    ( 8,  9, 10,  11,  12,  13,  14),
-    (15, 16, 17,  18,  19,  20,  21),
-    (22, 23, 24,  25,  26,  27,  28),
-    (29, 30, 31, u'', u'', u'', u''),
-]
-tpl = html.from_files(['layout.html', 'calendar.html'])
-tpl.render(Model(u"M\xe4rz", table))
+    def separate_col(self, node):
+        if node.ctx[0] != 3:
+            node.remove()
+
+
+tpl = _html.from_files(['layout.html', 'calendar.html'])
+tpl.render(Model(2014, 1))
